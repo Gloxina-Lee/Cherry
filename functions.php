40220052766e3444d4739343d12d3e164719a482
Gloxina <166108295+Gloxina-Lee@users.noreply.github.com>
2026-09-25 11:14:07 +0800

    移除AI相关功能

--- functions.php
+++ functions.php
@@ -1822,12 +1822,6 @@ function GBsubstr($string, $start, $length)
     }
 }
 
-/**
- * chatgpt excerpt
- */
-require_once __DIR__ . '/inc/chatgpt/hooks.php';
-IROChatGPT\apply_chatgpt_hook();
-
 function excerpt_length($exp)
 {
     if (!function_exists('mb_substr')) {
